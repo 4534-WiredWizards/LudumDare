8efa3c5ee2664052855074bb804c811124d5196d
Brandon Dyer <brandondyer64@gmail.com>
2015-12-11 23:06:48 -0500

    Signed-off-by: Brandon Dyer <brandondyer64@gmail.com>

---
+++ core/src/org/wiredwizards/ludum/dare/GameObject.java
@@ -0,0 +1,11 @@
+package org.wiredwizards.ludum.dare;
+
+public abstract class GameObject {
+	
+	public float x, y, width, height;
+	
+	public GameObject(float x, float y, float width, float height) {
+		this.x = x;
+	}
+	
+}
--- core/src/org/wiredwizards/ludum/dare/LD34Main.java
+++ core/src/org/wiredwizards/ludum/dare/LD34Main.java
@@ -1,16 +1,24 @@
 package org.wiredwizards.ludum.dare;
 
+import org.wiredwizards.ludum.dare.screens.TitleScreen;
+
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 
 public class LD34Main extends Game {
 	
 	@Override
 	public void create () {
-		
+		setScreen(new TitleScreen());
 	}
 
 	@Override
 	public void render () {
 		super.render();
+	}
+	
+	public static void setTheScreen(Screen screen) {
+		((Game)Gdx.app.getApplicationListener()).setScreen(screen);
 	}
 }
