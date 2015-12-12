e53583b28b4897d0f78f61869b7c4e998d2e0779
Brandon Dyer <brandondyer64@gmail.com>
2015-12-11 23:11:38 -0500

    Signed-off-by: Brandon Dyer <brandondyer64@gmail.com>

--- core/src/org/wiredwizards/ludum/dare/GameObject.java
+++ core/src/org/wiredwizards/ludum/dare/GameObject.java
@@ -1,11 +1,25 @@
 package org.wiredwizards.ludum.dare;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
 public abstract class GameObject {
-	
+
 	public float x, y, width, height;
-	
+
 	public GameObject(float x, float y, float width, float height) {
 		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
 	}
-	
+
+	public void render(SpriteBatch batch, float delta) {
+
+	}
+
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, width, height);
+	}
+
 }
