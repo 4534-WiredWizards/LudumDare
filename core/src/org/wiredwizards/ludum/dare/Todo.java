811e8a3a5db5f55c1856b74160326fa6ad6f61c1
Brandon Dyer <brandondyer64@gmail.com>
2015-12-11 22:39:21 -0500

    Signed-off-by: Brandon Dyer <brandondyer64@gmail.com>

---
+++ core/src/org/wiredwizards/ludum/dare/Todo.java
@@ -0,0 +1,23 @@
+package org.wiredwizards.ludum.dare;
+
+/**
+ * This is the todo list.
+ * 
+ * @author Me
+ *
+ */
+public interface Todo {
+	
+	// TODO: Austin
+	// TODO: Do art
+	
+	// TODO: Gage
+	// TODO: Make comprehensive design pitch on the Doc
+	
+	// TODO: Brandon
+	// TODO: Make screen system
+	
+	// TODO: Tom
+	// TODO: Make music
+
+}
