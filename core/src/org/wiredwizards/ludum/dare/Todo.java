4ddd4cf5ac310bb253764073ebfe4a916d93e52d
Brandon Dyer <brandondyer64@gmail.com>
2015-12-15 10:15:30 -0500

    Signed-off-by: Brandon Dyer <brandondyer64@gmail.com>

--- core/src/org/wiredwizards/ludum/dare/Todo.java
+++ core/src/org/wiredwizards/ludum/dare/Todo.java
@@ -8,17 +8,22 @@ package org.wiredwizards.ludum.dare;
  */
 public interface Todo {
 
-	// TODO: Austin
-	// TODO: Control systems
-
-	// TODO: Gage
-	// TODO: Make comprehensive design pitch on the Doc
-
 	// TODO: Brandon
+	// TODO: Add tilt controls
+	// TODO: Add options screen
+	// TODO: Fade music
+	// TODO: Level transitions
+	// TODO: Implement Gage's banner code
+	
+	// TODO: Gage
+	// TODO: Make AdMob and AdSense for Port City Robotics and send Brandon a banner code
 
 	// TODO: Tom
 	// TODO: Make music
 	// TODO: Make game object sprites
+	
+	// TODO: Benjamin
+	// TODO: Make music
 	
 	public void fixedError();
 
